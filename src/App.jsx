8ace01776ae53129ef709a14ec79f5809c98a312
Deepu <dpnr915@gmail.com>
2026-02-19 21:56:39 +0530

    added new components to parent layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import NavBar from './components/NavBar'
 import styled from '@emotion/styled';
 import SearchBox from './components/SearchBox';
 import Minicard from './components/Minicard';
+import BannerTempCard from './components/BannerTempCard';
+import HourlyForecastCard from './components/HourlyForecastCard';
 
 const Item = styled(Typography)(({ theme }) => ({
   backgroundColor: '#fff',
@@ -35,23 +37,30 @@ function App() {
 
   return (
     <>
-      <Grid container sx={{ backgroundColor: "neutral.900", height: "100vh", color: "white" }}>
+      <Grid container sx={{ backgroundColor: "neutral.900", height: "100vh", color: "white" }} >
         <NavBar />
-        <Grid size={12}>
+        <Grid size={12} sx={{ border: "2px solid white" }}>
           <Item>BANNER HEADING AND SEARCH BOX COMES HERE</Item>
-          <Item2><SearchBox /></Item2>
+          <SearchBox />
         </Grid>
-        <Grid size={8} sx={{ display: "flex", flexDirection: "row", border: "2px solid red" }} gap={3}>
-          <Grid size={4}><Minicard title="Feels Like" value="64" /></Grid>
-          <Grid size={4}><Minicard title="Feels Like" value="64" /></Grid>
-          <Grid size={4}><Minicard title="Feels Like" value="64" /></Grid>
-          <Grid size={4}><Minicard title="Feels Like" value="64" /></Grid>
-          <Grid size={4}><Minicard title="Feels Like" value="64" /></Grid>
-          <Grid size={4}><Minicard title="Feels Like" value="64" /></Grid>
+        <Grid size={12} sx={{ border: "2px solid gold", display: "flex", flexDirection: "row" }}>
+          <Grid size={7} sx={{ display: "flex", flexDirection: "column", border: "2px solid red" }} gap={3}>
+            <BannerTempCard />
+            <Grid size={12} sx={{ display: "flex", flexDirection: "row", border: "2px solid red" }} gap={3}>
 
-        </Grid>
-        <Grid size={4}>
-          <Item>size=4</Item>
+              <Grid size={4}><Minicard title="Feels Like" value="64" /></Grid>
+              <Grid size={4}><Minicard title="Feels Like" value="64" /></Grid>
+              <Grid size={4}><Minicard title="Feels Like" value="64" /></Grid>
+              <Grid size={4}><Minicard title="Feels Like" value="64" /></Grid>
+
+
+
+            </Grid>
+          </Grid>
+          <Grid size={5} sx={{ border: "2px solid red", }}>
+            <HourlyForecastCard />
+          </Grid>
+
         </Grid>
 
       </Grid>
