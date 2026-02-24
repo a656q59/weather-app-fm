0aa5d11e11c764e79ef4fb6941c071d265b689f5
Deepu <dpnr915@gmail.com>
2026-02-24 22:11:30 +0530

    created a container to practise react design patterns

--- src/containers/HourlyForecastContainer.jsx
+++ src/containers/HourlyForecastContainer.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import HourlyForecastCard from '../components/HourlyForecastCard'
+import { Box, Grid, Typography } from '@mui/material'
 
 export default function HourlyForecastContainer() {
     return (
-        <div>HourlyForecastContainer</div>
+        <Grid container
+            sx={(theme) => ({ display: "flex", flexDirection: "column", gap: 2, border: "2px solid red", height: "100%", padding: "0px 10px", backgroundColor: "neutral.700" })}>
+            <Typography>
+                Hourly Forecast
+            </Typography>
+            <HourlyForecastCard />
+            <HourlyForecastCard />
+        </Grid>
     )
 }
