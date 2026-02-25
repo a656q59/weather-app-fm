e6354ea6785d01134c5eecf8b258eae95724299d
Deepu <dpnr915@gmail.com>
2026-02-25 22:39:01 +0530

    setting the UI Layout

--- src/containers/HourlyForecastContainer.jsx
+++ src/containers/HourlyForecastContainer.jsx
@@ -5,7 +5,15 @@ import { Box, Grid, Typography } from '@mui/material'
 export default function HourlyForecastContainer() {
     return (
         <Grid container
-            sx={(theme) => ({ display: "flex", flexDirection: "column", gap: 2, border: "2px solid red", height: "100%", padding: "0px 10px", backgroundColor: "neutral.700" })}>
+            sx={(theme) => ({
+                display: "flex",
+                flexDirection: "column",
+                gap: 2,
+                borderRadius: "20px",
+                height: "100%",
+                padding: "0px 20px",
+                backgroundColor: "neutral.700"
+            })}>
             <Typography>
                 Hourly Forecast
             </Typography>
