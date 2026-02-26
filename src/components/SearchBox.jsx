cc422419f8185ec9d36124935fb8b2b44714b273
Deepu <dpnr915@gmail.com>
2026-02-26 22:19:44 +0530

    fixed gap issues

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -90,7 +90,7 @@ export default function SearchBox() {
 
   return (
     <Box sx={{ width: 600 }}>
-      <Box sx={{ display: "flex", gap: 2, }}>
+      <Box sx={{ display: "flex", gap: 1.5, }}>
         <Autocomplete
           fullWidth
           options={options}
