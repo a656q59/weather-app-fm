84e89db24a535a172cf89048f72e032bd02e2f28
Deepu <dpnr915@gmail.com>
2026-02-26 22:20:02 +0530

    fixing layout issues

--- src/containers/HourlyForecastContainer.jsx
+++ src/containers/HourlyForecastContainer.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import HourlyForecastCard from '../components/HourlyForecastCard'
 import { Box, Grid, Typography } from '@mui/material'
 
-export default function HourlyForecastContainer() {
+export default function HourlyForecastContainer({ data }) {
+    console.log("data inside hoursly container", data)
     return (
         <Grid container
             sx={(theme) => ({
@@ -11,7 +12,8 @@ export default function HourlyForecastContainer() {
                 gap: 2,
                 borderRadius: "20px",
                 height: "100%",
-                padding: "0px 20px",
+                marginLeft: "3px",
+                padding: "20px 22px",
                 backgroundColor: "neutral.700"
             })}>
             <Typography>
