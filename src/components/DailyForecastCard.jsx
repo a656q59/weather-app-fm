b09c6c3c413dac5d1bda86b3e46b41e0f4757cc1
Deepu <dpnr915@gmail.com>
2026-03-08 17:46:36 +0530

    created a new component to show daily forecast info

---
+++ src/components/DailyForecastCard.jsx
@@ -0,0 +1,46 @@
+import { Box, Typography } from '@mui/material'
+import React from 'react'
+
+export default function DailyForecastCard({ img, day = "Tue", minTemp = "20", maxTemp = "30" }) {
+    return (
+        <Box
+            sx={(theme) => ({
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "space-between",
+                padding: "10px 20px",
+                backgroundColor: "neutral.800",
+                borderRadius: "10px",
+                maxWidth: "100px"
+
+            })}
+        >
+            <Typography sx={{
+                marginBottom: "20px"
+            }}>
+                {day}
+            </Typography>
+
+            <img src={img} width="40px" height="40px" alt="image" />
+
+            <Box sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-between",
+                flexDirection: "row",
+                width: "100%",
+                marginTop: "20px"
+            }}>
+                <Typography>
+                    {minTemp}
+                </Typography>
+                <Typography>
+                    {maxTemp}
+                </Typography>
+            </Box>
+
+
+        </ Box>
+    )
+}
