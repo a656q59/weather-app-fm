aec9f7b0bd18caec8990081bb4688614383fb21b
Deepu <dpnr915@gmail.com>
2026-03-30 22:29:17 +0530

    passing loading props to minicard seems like bad idea

--- src/components/Minicard.jsx
+++ src/components/Minicard.jsx
@@ -1,7 +1,32 @@
-import { Box } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 import React from 'react'
 
-export default function Minicard({ title, value }) {
+export default function Minicard({ title, value, loading }) {
+    if (loading) return
+
+    (
+        <Box sx={(theme) => ({
+            backgroundColor: theme.palette.neutral.main,
+            display: "flex",
+            borderRadius: "13px",
+            flexDirection: "column",
+            padding: "14px 20px",
+            // maxWidth: "190px",
+            width: "100%",
+
+        })}>
+
+            <Typography sx={{ fontSize: "10px", marginBottom: "7px" }}>{title}</Typography>
+
+
+            <Typography sx={{
+                color: "white"
+            }}>
+                --
+            </Typography>
+        </Box>
+    )
+
     return (
         <Box sx={(theme) => ({
             backgroundColor: theme.palette.neutral.main,
