88453d5b4e0e8fa0d6b21c0dca175cffe868c1e5
Deepu <dpnr915@gmail.com>
2026-04-22 23:51:43 +0530

    debugging errors

--- src/components/DailyForecastCard.jsx
+++ src/components/DailyForecastCard.jsx
@@ -5,7 +5,7 @@ export default function DailyForecastCard({ loading, img, day = "Tue", minTemp =
     if (loading) return <Box
         sx={{
             color: "white",
-            border: "2px solid blue"
+            border: "2px solid green"
         }}>loading...</Box>
     return (
         <Box
