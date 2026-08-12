272c2f6e70e65b11392df9548f43641833276be5
Deepu <dpnr915@gmail.com>
2026-08-12 13:02:07 +0530

    removed logs

--- src/components/ui/Dropdown.jsx
+++ src/components/ui/Dropdown.jsx
@@ -1,59 +1,55 @@
-import React, { useState } from 'react'
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
-import { MenuItem } from '@mui/material';
+import React, { useState } from "react";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import { MenuItem } from "@mui/material";
 
 export default function Dropdown({ value = -1, onClick }) {
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-    const handleOpen = () => {
-        setOpen(true)
-    }
-    const handleClose = () => {
-        setOpen(false)
-    }
+  const [open, setOpen] = useState(false);
 
-
-    console.log("ifo")
-    const [open, setOpen] = useState(false);
-
-    return (
-        <div>
-            <FormControl sx={{ m: 1, minWidth: 120 }}>
-                <Select
-                    MenuProps={{
-                        PaperProps: {
-                            sx: {
-                                backgroundColor: "neutral.700",
-                                color: "white"
-                            },
-                        },
-                    }}
-                    sx={{
-                        "& .MuiInputBase-input": {
-                            padding: "5px 10px ",
-                            color: "white"
-                        },
-                        "& .MuiPaper-root": {
-                            backgroundColor: "blue"
-                        }
-                    }}
-                    open={open}
-                    onClose={handleClose}
-                    onOpen={handleOpen}
-                    value={value}
-
-                    onChange={(e) => onClick(e.target.value)}
-                >
-                    <MenuItem value={-1}></MenuItem>
-                    <MenuItem value={0}>Sunday</MenuItem>
-                    <MenuItem value={1}>Monday</MenuItem>
-                    <MenuItem value={2}>Tuesday</MenuItem>
-                    <MenuItem value={3}>Wednesday</MenuItem>
-                    <MenuItem value={4}>Thursday</MenuItem>
-                    <MenuItem value={5}>Friday</MenuItem>
-                    <MenuItem value={6}>Saturday</MenuItem>
-                </Select>
-            </FormControl>
-        </div>
-    )
+  return (
+    <div>
+      <FormControl sx={{ m: 1, minWidth: 120 }}>
+        <Select
+          MenuProps={{
+            PaperProps: {
+              sx: {
+                backgroundColor: "neutral.700",
+                color: "white",
+              },
+            },
+          }}
+          sx={{
+            "& .MuiInputBase-input": {
+              padding: "5px 10px ",
+              color: "white",
+            },
+            "& .MuiPaper-root": {
+              backgroundColor: "blue",
+            },
+          }}
+          open={open}
+          onClose={handleClose}
+          onOpen={handleOpen}
+          value={value}
+          onChange={(e) => onClick(e.target.value)}
+        >
+          <MenuItem value={-1}></MenuItem>
+          <MenuItem value={0}>Sunday</MenuItem>
+          <MenuItem value={1}>Monday</MenuItem>
+          <MenuItem value={2}>Tuesday</MenuItem>
+          <MenuItem value={3}>Wednesday</MenuItem>
+          <MenuItem value={4}>Thursday</MenuItem>
+          <MenuItem value={5}>Friday</MenuItem>
+          <MenuItem value={6}>Saturday</MenuItem>
+        </Select>
+      </FormControl>
+    </div>
+  );
 }
