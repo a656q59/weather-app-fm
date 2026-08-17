7a8a47225d55571f40aa571e2e5531edd834def6
Deepu <dpnr915@gmail.com>
2026-08-17 23:58:35 +0530

    log messages

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,45 +1,70 @@
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import { useTheme } from '@mui/material';
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import { useTheme } from "@mui/material";
 // import MenuIcon from '@mui/icons-material/Menu';
-import logo from "../assets/images/logo.svg"
-import units from "../assets/images/icon-units.svg"
-import dropdown from "../assets/images/icon-dropdown.svg"
+import logo from "../assets/images/logo.svg";
+import units from "../assets/images/icon-units.svg";
+import dropdown from "../assets/images/icon-dropdown.svg";
 
 export default function ButtonAppBar() {
-    const theme = useTheme();
+  const theme = useTheme();
 
-    return (
-        <Box sx={{ flexGrow: 1, }}>
-            <AppBar position="static" sx={{ backgroundColor: "neutral.900", paddingTop: "10px", paddingBottom: "10px", display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
-                <IconButton
-                    size="large"
-                    color="theme.palette.neutral.700"
-                    aria-label="menu"
-                    sx={{ mr: 2, flexGrow: 1, display: "flex", justifyContent: "start", padding: "0px", }}
-                >
-                    <img src={logo} />
-                </IconButton>
-                <Typography variant="h6" component="div">
-
-                </Typography>
-                <Button color="neutral"
-                    variant="contained"
-                    startIcon={
-                        <img src={units} />
-                    }
-                    endIcon={
-                        <img src={dropdown} />
-                    }
-
-                >
-                    Units
-                </Button>
-            </AppBar>
-        </Box >
-    );
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar
+        position="static"
+        sx={{
+          backgroundColor: "neutral.900",
+          paddingTop: "10px",
+          paddingBottom: "10px",
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+        }}
+      >
+        <IconButton
+          size="large"
+          color="theme.palette.neutral.700"
+          aria-label="menu"
+          sx={{
+            mr: 2,
+            flexGrow: 1,
+            display: "flex",
+            justifyContent: "start",
+            padding: "0px",
+          }}
+        >
+          <img src={logo} />
+        </IconButton>
+        <Typography variant="h6" component="div"></Typography>
+        <Button
+          color="neutral"
+          variant="contained"
+          startIcon={<img src={units} />}
+        >
+          <select
+            style={{
+              backgroundColor: "transparent",
+              border: "none",
+              color: "white",
+              fontSize: "16px",
+              fontWeight: "bold",
+              cursor: "pointer",
+            }}
+            name="units"
+            id="units"
+            value={units}
+            //   onChange={handleUnitsChange}
+          >
+            <option value="metric">Metric</option>
+            <option value="imperial">Imperial</option>
+          </select>
+        </Button>
+      </AppBar>
+    </Box>
+  );
 }
